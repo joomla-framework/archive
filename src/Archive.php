cd46f52274b25548a0368fda3fff2f0fed65be7d
Hannes Papenberg <info@joomlager.de>
2023-03-20 10:46:34 +0100

    Reverting change

--- src/Archive.php
+++ src/Archive.php
@@ -153,13 +153,13 @@ class Archive
      *
      * @return  $this
      *
-     * @throws  UnsupportedArchiveException if the adapter type is not supported
      * @since   1.0
+     * @throws  UnsupportedArchiveException if the adapter type is not supported
      */
     public function setAdapter($type, $class, $override = true)
     {
         if ($override || !isset($this->adapters[$type])) {
-            if (!class_exists($class)) {
+            if (!\is_object($class) && !class_exists($class)) {
                 throw new UnsupportedArchiveException($type, sprintf('Archive adapter "%s" (class "%s") not found.', $type, $class));
             }
 
