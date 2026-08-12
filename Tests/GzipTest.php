c9b9433df7b989c5bf7b931fad366c99fdd5a40b
Hannes Papenberg <info@joomlager.de>
2026-08-12 13:45:07 +0200

    Enable tests for bzip2 and gzip again - fix in filesystem package

--- Tests/GzipTest.php
+++ Tests/GzipTest.php
@@ -68,8 +68,6 @@ class GzipTest extends ArchiveTestCase
      */
     public function testExtractWithStreams()
     {
-        $this->markTestSkipped('There is a bug, see https://bugs.php.net/bug.php?id=63195&edit=1');
-
         if (!ArchiveGzip::isSupported()) {
             $this->markTestSkipped('Gzip files can not be extracted.');
         }
